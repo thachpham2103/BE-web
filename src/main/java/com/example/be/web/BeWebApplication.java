a3974c5e2de19d7dc4b6cf8cc2d40a512f385528
thachpham2103 <phamthach2103@gmail.com>
2025-08-14 01:53:05 +0700

    first commit

---
+++ src/main/java/com/example/be/web/BeWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.be.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeWebApplication.class, args);
+	}
+
+}
