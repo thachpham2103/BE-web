7a483c19d740a2e3ff13450ecde407e6071221e0
thachpham2103 <phamthach2103@gmail.com>
2025-08-22 23:25:32 +0700

    feat: create file exception

--- src/main/java/com/example/be/web/doman/mapper/UserMapper.java
+++ src/main/java/com/example/be/web/doman/mapper/UserMapper.java
@@ -1,5 +1,25 @@
 package com.example.be.web.doman.mapper;
 
+import com.example.be.web.doman.entity.User;
+import com.example.be.web.doman.request.UserCreateDto;
+import com.example.be.web.doman.response.UserResponseDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+
 public interface UserMapper {
+
+    @Mapping(source = "role.name", target = "roleName")
+    UserResponseDto toUserResponseDto(User user);
+
+    @Mapping(target = "role", ignore = true)
+    User toUser(UserCreateDto userCreateDto);
+
+    List<UserResponseDto> toUserResponseDtos(List<User> user);
+
+
 
 }
