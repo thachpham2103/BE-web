6a84f375627554d21651ffd1425c4c4bc7108ac4
thachpham2103 <phamthach2103@gmail.com>
2026-01-15 23:26:16 +0700

    feat: check and create data new

--- src/main/java/com/example/be/web/doman/entity/FaceData.java
+++ src/main/java/com/example/be/web/doman/entity/FaceData.java
@@ -1,44 +1,42 @@
 package com.example.be.web.doman.entity;
 
+import com.example.be.web.doman.entity.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
 
+@Entity
+@Table(name = "face_data")
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
-@Entity
-@Table(name="face_data")
 
 public class FaceData {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long facedataId;
+    private Long id;
 
-    @Column(name="image_url")
+    @Column(name = "image_url")
     private String imageUrl;
 
-    @Column(name="create_at", nullable = false)
-    private LocalDateTime createAt;
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
 
     @Lob
-    @Column(name="face_encoding", nullable = false, columnDefinition = "TEXT")
+    @Column(name = "face_encoding", nullable = false, columnDefinition = "TEXT")
     private String faceEncoding;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(
-            name="user_id",
-            referencedColumnName = "id",
-            foreignKey = @ForeignKey(name="FK_USER_ID")
+            name = "user_id",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "FK_FACE_DATA_USER")
     )
+    @JsonIgnore
     private User user;
-
-
-
 }
